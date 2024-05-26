9433409eb024bb93a45c05eb0708b0613a7da76a
Леонид Иванов <ivanov.l@robotkarl.com>
2024-05-26 22:55:33 +0800

    test

--- js/calcSumByClient.ts
+++ js/calcSumByClient.ts
@@ -1,11 +1,13 @@
 export const productTypesTemplate = [
   { name: 'Диагностика 7500₽', code: 'Diagnost', price: 7500 },
   { name: 'Диагностика+коррекция+подарок 15.000₽', code: 'DiagnostPlusCorr', price: 15000 },
+  { name: 'Тест оплаты* не платить', code: 'test', price: 500 },
 ]
 
 export const prices = {
   'Diagnost': 7500,
   'DiagnostPlusCorr': 7500,
+  'test': 500,
 }
 
 export function calcSumByClient(arr: any[]): number{
