4cbce3102ca70aef7b0b5a2fbb83760780a2f798
Леонид Иванов <ivanov.l@robotkarl.com>
2024-05-27 03:56:18 +0800

    s

--- pages/admin/paymentList.vue
+++ pages/admin/paymentList.vue
@@ -2,6 +2,29 @@
   <AdminLayout>
     <MainLayout>
       {{ data }}
+      <DataTable :value="arr" paginator :rows="15" :rowsPerPageOptions="[5, 10, 20, 50]"
+                 tableStyle="min-width: 50rem">
+        <Column header="Ордер ID">
+          <template #body="slotProps">
+            {{ slotProps.data.order_id }}
+          </template>
+        </Column>
+        <Column field="sum" header="Сумма"></Column>
+        <Column field="customer_phone" header="Сумма"></Column>
+        <Column field="customer_email" header="Сумма"></Column>
+        <Column field="customer_extra" header="Сумма"></Column>
+        <Column field="payment_type" header="Статус"></Column>
+        <Column header="Создан">
+          <template #body="slotProps">
+            <div class="flex align-items-center gap-2">
+              <span>{{
+                  slotProps.data.date ? new Date(slotProps.data.date).toLocaleString('ru-RU') : ''
+                }}</span>
+            </div>
+          </template>
+        </Column>
+      </DataTable>
+
     </MainLayout>
   </AdminLayout>
 </template>
@@ -14,9 +37,17 @@ import {useNuxtApp} from "nuxt/app";
 import {useToast} from "primevue/usetoast";
 import {useConfirm} from "primevue/useconfirm";
 import {calcSumByClient} from '@/js/calcSumByClient'
+import {computed} from "vue";
 
 const {data} = useApi('/payment/getPaymentList', {
   method: 'get'
+})
+
+const arr = computed(() => {
+  if (!Array.isArray(data.value)) {
+    return []
+  }
+  return data.value.map(el => JSON.parse(el.json))
 })
 </script>
 
